794554515a18ea0a1cba52e4f4c7d336c3b60acf
zxfjd3g <258147149@qq.com>
2017-09-02 10:58:27 +0800

    news_image_block组件

--- src/components/news_container.js
+++ src/components/news_container.js
@@ -5,6 +5,8 @@ import React, {Component} from 'react'
 import {Row, Col, Carousel, Tabs} from 'antd'
 
 import NewsBlock from './news_block'
+import NewsImageBlock from './news_image_block'
+import NewsProducts from './news_products'
 
 import carousel_1 from '../images/carousel_1.jpg'
 import carousel_2 from '../images/carousel_2.jpg'
@@ -27,6 +29,7 @@ export default class NewsContainer extends Component {
                 <div><img src={carousel_3}/></div>
                 <div><img src={carousel_4}/></div>
               </Carousel>
+              <NewsImageBlock type="guoji" count={6} cardTitle="国际新闻" cardWidth="100%" imageWidth='112px'></NewsImageBlock>
             </div>
             <Tabs className='tabs_news' style={{width:'35%'}}>
               <TabPane key="1" tab="头条新闻">
@@ -36,6 +39,17 @@ export default class NewsContainer extends Component {
                 <NewsBlock type="guoji" count={21}></NewsBlock>
               </TabPane>
             </Tabs>
+
+            <Tabs style={{width:'30%'}}>
+              <TabPane key="1" tab="React News产品">
+                <NewsProducts></NewsProducts>
+              </TabPane>
+            </Tabs>
+
+            <div>
+              <NewsImageBlock type="guonei" count={8} cardTitle="国内新闻" cardWidth="100%" imageWidth='132px'></NewsImageBlock>
+              <NewsImageBlock type="yule" count={16} cardTitle="娱乐新闻" cardWidth="100%" imageWidth='132px'></NewsImageBlock>
+            </div>
           </Col>
           <Col span={1}></Col>
         </Row>
---
+++ src/components/news_image_block.js
@@ -0,0 +1,80 @@
+/**
+ * 图片新闻列表组件
+ */
+import React, {Component, PropTypes} from 'react'
+import {Card} from 'antd'
+import {Link} from 'react-router'
+import axios from 'axios'
+
+export default class NewsImageBlock extends Component {
+
+  static propTypes = {
+    type: PropTypes.string.isRequired,
+    count: PropTypes.number.isRequired,
+    cardTitle: PropTypes.string.isRequired,
+    cardWidth: PropTypes.string.isRequired,
+    imageWidth: PropTypes.string.isRequired
+  }
+
+  state = {
+    newsArr: null
+  }
+
+  componentDidMount () {
+    // 发送ajax请求, 得到新闻列表数据
+    const {type, count} = this.props
+    const url = `http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=${type}&count=${count}`
+    axios.get(url)
+      .then(response => {
+        const newsArr = response.data.map(
+          ({uniquekey, title, author_name, thumbnail_pic_s})=> ({uniquekey, title, author_name,thumbnail_pic_s})
+        )
+        // 更新状态
+        this.setState({newsArr})
+      })
+  }
+
+  render () {
+    const {cardTitle, cardWidth, imageWidth} = this.props
+    const {newsArr} = this.state
+    // 图片样式对象
+    const imageStyle = {
+      width: imageWidth,
+      height: '90px',
+      display: 'block'
+    }
+
+    // 标题样式对象
+    const titleStyle = {
+      "width": imageWidth,
+      "whiteSpace": "nowrap",
+      "overflow": "hidden",
+      "textOverflow": "ellipsis"
+    }
+
+    const newsList = !newsArr
+      ? <h2>没有任意新闻</h2>
+      : (
+          newsArr.map((news, index) => (
+            <div key={index} className="imageblock">
+              <Link to={`/news_detail/${news.uniquekey}`}>
+                <div>
+                  <img src={news.thumbnail_pic_s} style={imageStyle}/>
+                </div>
+                <div className="custom-card">
+                  <h3 style={titleStyle}>{news.title}</h3>
+                  <p>{news.author_name}</p>
+                </div>
+              </Link>
+            </div>
+          ))
+        )
+    return (
+      <Card title={cardTitle} style={{width:cardWidth}} className="topNewsList">
+        {
+          newsList
+        }
+      </Card>
+    )
+  }
+}
