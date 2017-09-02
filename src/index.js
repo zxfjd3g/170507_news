3f42af2e8ddc4cb1cfc06626769ada0fca7929a9
zxfjd3g <258147149@qq.com>
2017-09-02 14:05:47 +0800

    MediaQuery

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Router, Route, hashHistory, IndexRoute} from 'react-router'
+import MediaQuery from 'react-responsive'
 import App from './components/app'
 import NewsContainer from './components/news_container'
 import NewsDetail from './components/news_detail'
@@ -8,11 +9,18 @@ import UserCenter from './components/user_center'
 
 
 render((
-  <Router history={hashHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={NewsContainer}></IndexRoute>
-      <Route path='/news_detail/:id' component={NewsDetail}></Route>
-      <Route path='/user_center' component={UserCenter}></Route>
-    </Route>
-  </Router>
+  <div>
+    <MediaQuery query='(min-device-width: 1224px)'>
+      <Router history={hashHistory}>
+        <Route path='/' component={App}>
+          <IndexRoute component={NewsContainer}></IndexRoute>
+          <Route path='/news_detail/:id' component={NewsDetail}></Route>
+          <Route path='/user_center' component={UserCenter}></Route>
+        </Route>
+      </Router>
+    </MediaQuery>
+    <MediaQuery query='(max-device-width: 1224px)'>
+      <div>手机界面</div>
+    </MediaQuery>
+  </div>
 ), document.getElementById('root'))
